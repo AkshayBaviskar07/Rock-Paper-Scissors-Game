e42360832c3c6064853c47d660647f0d18211070
akshay <baviskarakshay0702@gmail.com>
2025-04-18 15:54:53 +0530

    modified

--- js/script.js
+++ js/script.js
@@ -15,40 +15,40 @@ function playGame(playerMove) {
   // rock
   if (playerMove === 'rock') {
     if (computerMove === 'rock') {
-      result = 'Tie.'
+      result = 'Tie'
     } else if (computerMove === 'paper') {
-      result = 'You lose.';
+      result = 'You lose';
     } else if (computerMove === 'scissors') {
-      result = 'You win.'
+      result = 'You win'
     }
   }
 
   //paper
   if (playerMove === 'paper') {
     if (computerMove === 'rock') {
-      result = 'You win.';
+      result = 'You win';
     } else if (computerMove === 'paper') {
-      result = 'Tie.'
+      result = 'Tie'
     } else if (computerMove === 'scissors') {
-      result = 'You lose.'
+      result = 'You lose'
     }
   }
 
   //scissors
   if (playerMove === 'scissors') {
     if (computerMove === 'rock') {
-      result = 'You lose.';
+      result = 'You lose';
     } else if (computerMove === 'paper') {
       result = 'You win';
     } else if (computerMove === 'scissors') {
-      result = 'Tie.';
+      result = 'Tie';
     }
   }
 
 
   if (result === 'You win') {
     score.wins += 1; 
-  } else if (result === 'You lose.') {
+  } else if (result === 'You lose') {
     score.losses += 1;
   } else if (result === 'Tie') {
     score.ties += 1;
@@ -70,6 +70,11 @@ function playGame(playerMove) {
   <img src="images/${computerMove}-emoji.png" class="move-icon"/> Computer`;
 }
 
+function updateScoreElement() {
+  document.querySelector('.js-score')
+  .innerHTML = `Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`;
+};
+
 // pick computer move
 function pickComputerMove() {
   const randomNumber = Math.random();
@@ -84,10 +89,5 @@ function pickComputerMove() {
   }
 
   return computerMove;
-}
-
-function updateScoreElement() {
-  document.querySelector('.js-score')
-  .innerHTML = `Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`;
-}
+};
 
